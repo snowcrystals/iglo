e90fa857185a5176371ccce76e9a119923c1e9b5
ijsKoud <github@ijskoud.dev>
2022-12-03 22:55:09 +0100

    refactor: src/* -> src/lib/*

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-export * from "./decorators/index.js";
-export * from "./Errors/index.js";
-export * from "./handlers/index.js";
-export * from "./structures/index.js";
-export * from "./types.js";
-export * from "./Client.js";
-
+export * from "./lib/index.js";
 export * from "@snowcrystals/icicle";
---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export * from "./decorators/index.js";
+export * from "./Errors/index.js";
+export * from "./handlers/index.js";
+export * from "./structures/index.js";
+export * from "./types.js";
+export * from "./Client.js";
