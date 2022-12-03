b526b693351a29ffb638b21fea622ab87ba30e51
ijsKoud <github@ijskoud.dev>
2022-12-03 20:11:05 +0100

    feat(CommandRegistryHandler): add difference type as string to result

--- src/handlers/CommandRegistryHandler.ts
+++ src/handlers/CommandRegistryHandler.ts
@@ -151,51 +151,55 @@ export class CommandRegistry {
 		if (!_.isEqual(discord.defaultMemberPermissions, command.permissions.default ? new PermissionsBitField(command.permissions.default) : null))
 			return "defaultMemberPermissions";
 
-		if (this.optionsAreDifferent(discord.options, command.options)) return "options";
+		const optionsRes = this.optionsAreDifferent(discord.options, command.options);
+		if (optionsRes) return `options(${optionsRes})`;
 
 		return null;
 	}
 
-	private optionsAreDifferent(discord: ApplicationCommandOption[], command: ApplicationCommandOption[]): boolean {
+	private optionsAreDifferent(discord: ApplicationCommandOption[], command: ApplicationCommandOption[]): string | null {
 		// Expected options but received no options
-		if (!discord.length && command.length) return true;
+		if (!discord.length && command.length) return "amount";
 		// Expected no options but received options
-		if (discord.length && !command.length) return true;
+		if (discord.length && !command.length) return "amount";
 
 		// Check to see if commands here include options which aren't at Discord
-		if (command.some((opt) => !discord.find((o) => o.name === opt.name))) return true;
+		if (command.some((opt) => !discord.find((o) => o.name === opt.name))) return "data-notEqual";
 		// Check to see if commands at Discord include options which aren't here
-		if (discord.some((opt) => !command.find((o) => o.name === opt.name))) return true;
+		if (discord.some((opt) => !command.find((o) => o.name === opt.name))) return "data-notEqual";
 
 		// Map over all options to see if they are the same
 		for (const option of command) {
 			const existingOption = discord.find((opt) => opt.name === option.name);
-			if (this.isOptionDifferent(existingOption, option)) return true;
+			const optionsRes = this.isOptionDifferent(existingOption, option);
+			if (optionsRes) return optionsRes;
 		}
 
-		return false;
+		return null;
 	}
 
-	private isOptionDifferent(discord: ApplicationCommandOption | undefined, command: ApplicationCommandOption): boolean {
+	private isOptionDifferent(discord: ApplicationCommandOption | undefined, command: ApplicationCommandOption): string | null {
 		// Expected DiscordOption but received undefined
-		if (!discord) return true;
+		if (!discord) return "undefined";
 
-		// check the name localizations
+		// Re-assigning nameLocalizations value to undefined if value is null
 		discord.nameLocalizations ??= undefined;
-		if (!_.isEqual(discord.nameLocalizations, command.nameLocalizations)) return true;
+		if (!_.isEqual(discord.nameLocalizations, command.nameLocalizations)) return "nameLocalizations";
+
 		// check the description localizations
-		if (!_.isEqual(discord.nameLocalizations, command.descriptionLocalizations)) return true;
+		if (!_.isEqual(discord.descriptionLocalizations, command.descriptionLocalizations)) return "descriptionLocalizations";
 		// check the description
-		if (discord.description !== command.description) return true;
+		if (discord.description !== command.description) return "description";
 
 		// Check the type
-		if (discord.type !== command.type) return true;
+		if (discord.type !== command.type) return "type";
 		// check if autocomplete is different
-		if ((discord.autocomplete ?? false) !== (command.autocomplete ?? false)) return true;
+		if ((discord.autocomplete ?? false) !== (command.autocomplete ?? false)) return "autocomplete";
+
 		// check if required
 		if ("required" in discord) {
 			const cmd = command as typeof discord;
-			if ((discord.required ?? false) !== (cmd.required ?? false)) return true;
+			if ((discord.required ?? false) !== (cmd.required ?? false)) return "required";
 		}
 
 		switch (discord.type) {
@@ -203,18 +207,20 @@ export class CommandRegistry {
 			case ApplicationCommandOptionType.Subcommand: {
 				const disc = discord as ApplicationCommandSubGroup;
 				const cmd = command as ApplicationCommandSubGroup;
-				return this.optionsAreDifferent(disc.options!, cmd.options!);
+				return this.optionsAreDifferent(disc.options ?? [], cmd.options ?? []);
 			}
 			case ApplicationCommandOptionType.String:
 				{
 					const cmd = command as ApplicationCommandStringOption | ApplicationCommandAutocompleteStringOption;
 					const disc = discord as ApplicationCommandStringOption | ApplicationCommandAutocompleteStringOption;
-					if (!_.isEqual(disc.maxLength, cmd.maxLength)) return true;
-					if (!_.isEqual(disc.minLength, cmd.minLength)) return true;
+					if (!_.isEqual(disc.maxLength, cmd.maxLength)) return "maxLength";
+					if (!_.isEqual(disc.minLength, cmd.minLength)) return "minLength";
 
 					// Expected choices but got undefined (and reversed)
-					if ("choices" in disc && !("choices" in cmd)) return true;
-					if (!("choices" in disc) && "choices" in cmd) return true;
+					// @ts-ignore Discord data returns undefined if no choices are passed, we should do the same with local data
+					cmd.choices ??= undefined;
+					if ("choices" in disc && !("choices" in cmd)) return "choices";
+					if (!("choices" in disc) && "choices" in cmd) return "choices";
 
 					if ("choices" in disc && "choices" in cmd) {
 						const discChoices = disc.choices ?? [];
@@ -224,16 +230,16 @@ export class CommandRegistry {
 							const discChoice = discChoices.find((c) => c.name === choice.name);
 							if (!discChoice) return true;
 
-							if (!_.isEqual(discChoice.nameLocalizations, choice.nameLocalizations)) return true;
-							if (typeof discChoice.value !== typeof choice.value) return true;
-							if (discChoice.value !== choice.value) return true;
+							if (!_.isEqual(discChoice.nameLocalizations, choice.nameLocalizations)) return "choices-nameLocalizations";
+							if (typeof discChoice.value !== typeof choice.value) return "choices-value";
+							if (discChoice.value !== choice.value) return "choices-value";
 
 							return false;
 						};
 
 						// one of the items has more than expected
-						if (discChoices.length !== cmdChoices.length) return true;
-						if (cmdChoices.some(someChoice)) return true;
+						if (discChoices.length !== cmdChoices.length) return "choices-amount";
+						if (cmdChoices.some(someChoice)) return "choices-notEqual";
 					}
 				}
 				break;
@@ -242,8 +248,8 @@ export class CommandRegistry {
 					const disc = discord as ApplicationCommandChannelOption;
 					const cmd = command as ApplicationCommandChannelOption;
 
-					if (disc.channelTypes?.length !== cmd.channelTypes?.length) return true;
-					if (!_.isEqual(disc.channelTypes, cmd.channelTypes)) return true;
+					if (disc.channelTypes?.length !== cmd.channelTypes?.length) return "channel-amount";
+					if (!_.isEqual(disc.channelTypes, cmd.channelTypes)) return "channel-channelTypes";
 				}
 				break;
 			case ApplicationCommandOptionType.Integer:
@@ -251,15 +257,15 @@ export class CommandRegistry {
 				{
 					const cmd = command as ApplicationCommandNumericOption | ApplicationCommandAutocompleteNumericOption;
 					const disc = discord as ApplicationCommandNumericOption | ApplicationCommandAutocompleteNumericOption;
-					if (!_.isEqual(disc.maxValue, cmd.maxValue)) return true;
-					if (!_.isEqual(disc.minValue, cmd.minValue)) return true;
+					if (!_.isEqual(disc.maxValue, cmd.maxValue)) return "int-maxValue";
+					if (!_.isEqual(disc.minValue, cmd.minValue)) return "int-minValue";
 				}
 				break;
 			default:
 				break;
 		}
 
-		return false;
+		return null;
 	}
 
 	/** Returns an object with all the necessary data to register a command */
